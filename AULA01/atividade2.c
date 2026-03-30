8fb714c5d0430522e04555007d9832c80e5e4c85
miguel <miguel.favero@aluno.senai.br>
2026-03-30 11:47:11 -0300

    alteração

---
+++ AULA01/atividade2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+float a,b,c,d;
+int main(){
+    printf("--MEDIA--\n\nVamos calcular sua media\n");
+    printf("\nfale sua primeira nota :\n");
+    scanf("%f",&a);
+    printf("\nfale sua segunda nota :\n");
+    scanf("%f",&b);
+    printf("\nfale sua terceira nota :\n");
+    scanf("%f",&c);
+    d = (a+b+c )/3;
+    printf("sua media foi %.1f",d);
+     if (d >6.9 ){
+         printf("\npassou parabens");
+     }else if (d>2.9){
+         printf("\nem recuperação");
+     }else {
+         printf("\nreprovado");
+     }
+return 0;
+}
